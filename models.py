a4e7e96f53a13a6351fc408db6648710d9e4c828
Vena Khamvanthong <venakhamvan@gmail.com>
2022-04-11 17:19:24 -0400

    Adding tables for user, notes, course, and comments

---
+++ models.py
@@ -0,0 +1,30 @@
+from click import password_option
+from flask_sqlalchemy import SQLAlchemy
+
+db=SQLAlchemy()
+
+class Users(db.Model):
+    user_id = db.column(db.Integer, primary_key=True)
+    username = db.column(db.String, nullable = False)
+    email = db.column(db.String, nullable = False)
+    password = db.column(db.String, nullable = False)
+
+class Notes(db.Model):
+    note_id = db.column(db.Integer, primary_key = True)
+    title = db.column(db.String, nullable = False)
+    description = db.columns(db.String, nullable = False)
+    content = db.column(db.String, nullable = False)
+    course_id = db.columns(db.String, db.ForeignKey('Courses.course_id'), nullable = False)
+    creator_id = db.column(db.Integer, db.ForeignKey('Users.user_id'), nullable = False)
+
+class Courses(db.Model):
+    course_id = db.column(db.Integer, primary_key = True)
+    title = db.column(db.String)
+    
+class Comments(db.Model):
+    comment_id = db.column(db.Integer, primary_key = True)
+    content = db.column(db.String, nullable = False)
+    timestamp = db.column(db.DateTime, nullable = False)
+    commenter_id = db.column(db.Integer, db.ForeignKey('Users.user_id'), nullable = False)
+    thread_id = db.column(db.String, db.ForeignKey('Notes.note_id'), nullable = False)
+    
