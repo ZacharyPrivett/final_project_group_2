413b3048f75489a2fdaa6116b2f34c5b1d47277d
schen2650 <sp.chen526@gmail.com>
2022-04-11 19:48:00 -0400

    updated table to include course

--- notes_app.sql
+++ notes_app.sql
@@ -2,6 +2,8 @@ CREATE DATABASE users;
 USE users;
 CREATE DATABASE note;
 USE note;
+CREATE DATABASE course;
+USE course;
 
 CREATE TABLE IF NOT EXISTS users(
 	user_id INT AUTO_INCREMENT,
@@ -14,10 +16,19 @@ CREATE TABLE IF NOT EXISTS users(
     
 CREATE TABLE IF NOT EXISTS note(
 	note_id INT AUTO_INCREMENT,
-    title VARCHAR(20) NOT NULL,
+    title VARCHAR(255) NOT NULL,
     course VARCHAR(255) NOT NULL,
     descript VARCHAR(255) NULL,
     creator_id INT NULL,
     PRIMARY KEY(note_id),
     FOREIGN KEY(creator_id) REFERENCES users(user_id)
     );
+
+CREATE TABLE IF NOT EXISTS course (
+    user_id  INT NOT NULL,
+    note_id  INT NOT NULL,
+    _name VARCHAR(255) NOT NULL,
+    PRIMARY KEY (user_id, note_id),
+    FOREIGN KEY (user_id) REFERENCES users(user_id),
+    FOREIGN KEY (note_id) REFERENCES note(note_id)
+);
