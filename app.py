226117fc83825da19d1ca155ae296286be31af54
will <willpspeight@gmail.com>
2022-04-17 18:52:31 -0400

    submitting signup page front end

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask, redirect, render_template, request
 from notes_repository import note_repository_singleton
 from models import db
 import os
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 
-load_dotenv()
+#load_dotenv()
 
 app = Flask(__name__)
 
@@ -18,15 +18,34 @@ db.init_app(app)
 def index():
     return render_template('index.html')
 
+
+
+@app.get('/add_notes')
+def add_notes():
+    return render_template('add_notes.html')
+
+@app.get('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.get('/signup')
+def signup():
+    return render_template('signup_page.html')
+
 @app.get('/notes')
 def create_notes():
     title = request.form.get('title', '')
     course = request.form.get('course', '')
-    description = request.get('description', '')
-    notes = request.get('notes', '')
-
+    #i changed these two lines of code for this to work for me
+    description = request.form.get('description', '')
+    notes = request.form.get('notes', '')
     create_notes = note_repository_singleton.create_notes(title, course, description, notes)
     return redirect(f'/notes/')
+
+@app.get('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
 
 
 @app.get('/search')
@@ -45,3 +64,7 @@ def view_all_notes():
 @app.get('/about')
 def about():
     return render_template('about.html')
+
+@app.get('/single_note')
+def single_note():
+    return render_template('single_note_page.html')
