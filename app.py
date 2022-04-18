bb8bd47c6db1e1efd03ecceb4b568b87fca5ccc1
ZacharyPrivett <zprivet0@email.cpcc.edu>
2022-04-18 16:44:05 -0400

    a

--- app.py
+++ app.py
@@ -35,8 +35,8 @@ def create_note():
     descript = request.form.get('descript', '')
     content = request.form.get('content', '')
     creator_id = 1  # so code doesnt break. Will update when we have login made
-    #if title == '' or course == '' or descript == '' or content == '':
-        #os.abort(400)
+    if title == '' or course == '' or descript == '' or content == '':
+        os.abort(400)
     created_notes = note_repository_singleton.create_note(title, course, descript, content, creator_id)
     return render_template('add_notes.html', search_active=True, new_notes=created_notes)
 
