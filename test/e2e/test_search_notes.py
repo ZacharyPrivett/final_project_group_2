e7db05b5be17b68fc6bbb9af2376d3c5f128137e
Adrian Mallari <A87kamui@gmail.com>
2022-05-10 20:51:19 -0400

    updating test search note

--- test/e2e/test_search_notes.py
+++ test/e2e/test_search_notes.py
@@ -1,7 +1,10 @@
 from flask.testing import FlaskClient
 
-def test_view_all_notes(test_app: FlaskClient):
-    response = test_app.get('/notes/list')
+def test_search_notes(test_app: FlaskClient):
+    response = test_app.get('/search')
     response_data = response.data
 
-    assert b'<p>No notes found</p>' in response_data
+    assert b'<option value="Title">Search by Title</option>' in response_data
+    assert b'<option value="Course">Search by Course</option>' in response_data
+    assert b'<option value="Author">Search by Author</option>' in response_data
+    assert b'<input type="text" name="q" id="q" class="form-control" autocomplete="off" required>' in response_data
